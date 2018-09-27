b0cb19502f0917f6c05548b099717895671cd14f
mark <markdamonrood@gmail.com>
2018-09-27 13:26:16 -0500

    added Ingredients and the relationship to recipe

--- src/main/java/rood/mark/domain/Recipe.java
+++ src/main/java/rood/mark/domain/Recipe.java
@@ -1,6 +1,9 @@
 package rood.mark.domain;
 
+import org.hibernate.engine.internal.Cascade;
+
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -22,6 +25,9 @@ public class Recipe {
     private Byte image;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
@@ -101,5 +107,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
