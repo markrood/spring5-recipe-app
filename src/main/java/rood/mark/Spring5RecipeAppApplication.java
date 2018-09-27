b6dcf611adcc9221b3f87188c51909a61ec4dd32
mark <markdamonrood@gmail.com>
2018-09-27 12:59:41 -0500

    added Notes and Recipe

--- src/main/java/rood/mark/Spring5RecipeAppApplication.java
+++ src/main/java/rood/mark/Spring5RecipeAppApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package rood.mark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/rood/mark/Spring5RecipeAppApplicationTests.java
+++ src/test/java/rood/mark/Spring5RecipeAppApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package rood.mark;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
